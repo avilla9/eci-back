5dba7f4b81df25e2c4ce142579bc928490f20656
Armando Villanueva <armandojvilla9@gmail.com>
2022-09-15 16:33:24 -0400

    user save

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -124,7 +124,7 @@ class UserController extends Controller {
             return response()->json($validator->errors(), 404);
         } else {
             $request->merge(['password' => Hash::make($request->password)]);
-            $user = User::where('dni', $request->id);
+            $user = User::where('dni', $request->id)->first();
             $user->dni = $request->dni;
             $user->name = $request->name;
             $user->gender = $request->gender;
