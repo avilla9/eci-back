35fd9a888367b842b7d12589b3efcb9557039ad0
Armando Villanueva <armandojvilla9@gmail.com>
2022-10-02 19:53:57 -0400

    backslash

--- database/migrations/2022_06_17_113519_create_uploads_table.php
+++ database/migrations/2022_06_17_113519_create_uploads_table.php
@@ -13,16 +13,11 @@ class CreateUploadsTable extends Migration {
     public function up() {
         Schema::create('uploads', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
+            $table->integer('user_id')->nullable();
             $table->string('filename');
             $table->softDeletes();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
         });
     }
 
