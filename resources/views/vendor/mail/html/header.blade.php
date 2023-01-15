593ee3f9cb4cc9a1bada5e80b272a42223ccbc65
Mmcaraballo <miguelalejandroc123@gmail.com>
2023-01-15 17:04:51 -0400

    email header logo changed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('img/logo.jpg') }}" class="logo" alt="El Corte Inglés Logo">
+<img src="{{ asset('file/logo.jpg') }}" class="logo" alt="El Corte Inglés Logo">
 @else
 {{ $slot }}
 @endif
