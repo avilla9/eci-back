9b963bc64e5d489374d04cf6919c498c98635738
GoldJackAnderson <99150916+GoldJackAnderson@users.noreply.github.com>
2023-02-26 17:33:16 -0400

    [NEW] CTA Files (#31)
    Agregado boton para seleccionar un archivo y añadir su URL al link del CTA de todos los articulos

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1004,7 +1004,9 @@ function contentParameters() {
     $delegations = Delegation::all();
     $roles = Role::all();
     $users = User::all();
-    $files = File::where('media_type', 'like', '%image%')->latest()->get();
+    $files = File::all();
+    $files = File::orderBy('updated_at', 'desc')->get();
+    // $files = File::where('media_type', 'like', '%image%')->latest()->get();
 
     $filters = [];
     count($groups) > 0 ? $filters['groups'] = [
